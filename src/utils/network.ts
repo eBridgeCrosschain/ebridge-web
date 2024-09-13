a42f4e082c8307ac09f785297a778d61619508b7
kled-bridge <mason@ebridge.exchange>
2024-09-13 16:18:07 +0800

    feat: 🎸 update network utils

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -13,6 +13,7 @@ import { NetworkType } from 'types';
 import { ChainId } from 'types';
 import { WalletConnect } from '@web3-react/walletconnect-v2';
 import CommonMessage from 'components/CommonMessage';
+import { NetworkList } from 'constants/index';
 
 type Info = {
   chainId: number | string;
@@ -165,4 +166,8 @@ export const switchChain = async (
     // unlink metamask
     switchNetwork(info);
   }
+};
+
+export const getNetworkInfo = (chainId: ChainId) => {
+  return NetworkList.find((info) => info.info.chainId === chainId);
 };
