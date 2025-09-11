7764096783c8359a079000bac1a7e73e3a291444
kled-bridge <mason@ebridge.exchange>
2025-09-11 14:28:36 +0800

    feat: 🎸 update config

--- src/components/Web3Provider/webLoginV2Provider.tsx
+++ src/components/Web3Provider/webLoginV2Provider.tsx
@@ -1,11 +1,18 @@
 'use client';
-
 // import '../../utils/telegram/telegram-web-app';
+import { useMemo } from 'react';
 import { WebLoginProvider } from '@aelf-web-login/wallet-adapter-react';
-import { config } from './webLoginV2Config';
+import { config, didConfig } from './webLoginV2Config';
 import { useEffect, useState } from 'react';
+import { did } from '@portkey/did';
+import { checkConnectedWallet } from 'utils/portkey';
 
 export default function WebLoginV2Providers({ children }: { children: React.ReactNode }) {
+  useMemo(() => {
+    did.setConfig(didConfig);
+    checkConnectedWallet();
+  }, []);
+
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     const script = document.createElement('script');
